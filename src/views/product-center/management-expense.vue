fca5c67cfd3d94f78502a4860a324d393925dba7
zhengjianli <zhengjianli126@qq.com>
2018-05-28 14:39:09 +0800

    费用管理

--- src/views/product-center/management-expense.vue
+++ src/views/product-center/management-expense.vue
@@ -5,48 +5,26 @@
 <template>
   <div style="padding:15px;background:#FFF;overflow:hidden">
     <i-button type="primary" @click="modal2 = true">显示对话框1</i-button>
-    <Modal width="800px" v-model="modal2" title="公式编辑" :loading="loading">
+    <Modal styles="z-index:1000;" width="800px" v-model="modal2" title="公式编辑" :loading="loading">
       <Row>
         <Col span="8">
-        <Menu :theme="theme2" accordion="true" style="min-height:100px;">
-          <Submenu name="1">
-            <template slot="title">
-                                    系统变量
-</template>
-                            <MenuItem name="1-1">贷款本金</MenuItem>
-                            <MenuItem name="1-2">贷款期数</MenuItem>
-                            <MenuItem name="1-3">逾期天数</MenuItem>
-                            <MenuItem name="1-4">当期应收本金</MenuItem>
-                        </Submenu>
-                        <Submenu name="2">
-<template slot="title">
-   系统函数
-</template>
-                            <MenuItem name="1-1">贷款本金</MenuItem>
-                            <MenuItem name="1-2">贷款期数</MenuItem>
-                            <MenuItem name="1-3">逾期天数</MenuItem>
-                            <MenuItem name="1-4">当期应收本金</MenuItem>
-                        </Submenu>
-                          <Submenu name="2">
-<template slot="title">
-   系统函数
-</template>
-                            <MenuItem name="1-1">贷款本金</MenuItem>
-                            <MenuItem name="1-2">贷款期数</MenuItem>
-                            <MenuItem name="1-3">逾期天数</MenuItem>
-                            <MenuItem name="1-4">当期应收本金</MenuItem>
-                        </Submenu>
-                          <Submenu name="2">
-<template slot="title">
-   系统函数
-</template>
-                            <MenuItem name="1-1">贷款本金</MenuItem>
-                            <MenuItem name="1-2">贷款期数</MenuItem>
-                            <MenuItem name="1-3">逾期天数</MenuItem>
-                            <MenuItem name="1-4">当期应收本金</MenuItem>
-                        </Submenu>
-                    </Menu>
-                </Col>
+          <Menu @on-select="route" :theme="theme2" accordion="true" style="min-height:100px;">
+            <Submenu name="1">
+              <template slot="title">系统变量</template>
+              <MenuItem v-for="item in findAllSystemVariables" :name="item.label+'-'+item.value">{{item.label}}</MenuItem>
+            </Submenu>
+            <Submenu name="2">
+              <template slot="title">系统函数</template>
+                <MenuItem v-for="item in findAllSystemFunction" :name="item.label">{{item.label}}</MenuItem>
+               
+            </Submenu>
+             <Submenu name="3">
+              <template slot="title">运算符号</template>
+                <MenuItem v-for="item in ysfhJs" :name="item.label">{{item.label}}</MenuItem>
+               
+            </Submenu>
+          </Menu>
+        </Col>
                <Col span="12">
                    <span>输入公式：</span>
                     <Input style="margin-top:10px;" v-model="srgs" type="textarea" placeholder="Enter something..."></Input>
@@ -58,8 +36,8 @@
                </Col>
             </Row>
             <div slot="footer">
-                <Button type="primary">验证</Button>
-                <Button type="primary">保存</Button>
+                <Button type="primary" @click="vildGs()">验证</Button>
+                <Button type="primary"@click="submitGs()">保存</Button>
             </div>
         </Modal>
 
@@ -67,70 +45,84 @@
     <i-button type="primary" small @click="lookFindAllFee">查看费用</i-button>
     <i-button type="primary" small @click="addFindAllFee">新增费用</i-button>
     <!-- 费用弹框 -->
-        <Modal width="800px" v-model="modal1" :title="titleDig"
+        <Modal  width="800px" v-model="modal1" :title="titleDig"
         :loading="loading">
-
+        <Form ref="FeeInfo" :model="FeeInfo" :label-width="100"  :rules="digValidate">
           <Row>
             <Col span="12">
-                <span>费用名称：</span><i-input :disabled="digFlag" v-model="FeeInfo.feeName" placeholder="请输入..." style="width: 200px"></i-input>
+                <FormItem label="费用名称："  prop="feeName">
+                <i-input :disabled="digFlag" v-model="FeeInfo.feeName" placeholder="请输入..." style="width: 200px"></i-input>
+                </FormItem>
             </Col>
+             
              <Col span="12">
-                <span>费用类型：&nbsp;&nbsp;&nbsp;</span><i-select  :disabled="digFlag" @on-change="digFeeTypeJs"   v-model="FeeInfo.feeType" span="6" style="width:200px">
-          <i-option v-for="item in findAllFeeTypeList" :value="item.value">{{ item.label }}</i-option>
-        </i-select>
+                <FormItem label="费用类型：" prop="feeType">
+                  <i-select  :disabled="digFlag" @on-change="digFeeTypeJs"   v-model="FeeInfo.feeType" span="6" style="width:200px">
+                    <i-option v-for="item in findAllFeeTypeList" :value="item.value">{{ item.label }}</i-option>
+                </i-select>
+              </FormItem>
             </Col>
           </Row>
            <Row style="margin-top:10px;">
              <Col span="12">
-                <span>费用大类：</span> <i-select v-model="FeeInfo.feeCategory" :disabled="digFlag"   span="6" style="width:200px">
-        <i-option v-for="item in findAllCostCategory" :value="item.value">{{ item.label }}</i-option>
-        </i-select>
+                <FormItem label="费用大类：" prop="feeCategory">
+                <i-select v-model="FeeInfo.feeCategory" :disabled="digFlag"   span="6" style="width:200px">
+                <i-option v-for="item in findAllCostCategory" :value="item.value">{{ item.label }}</i-option>
+                </i-select>
+                </FormItem>
             </Col>
               <Col span="12">
-                <span>费用项：</span><i-input v-model="FeeInfo.feeTag"  :disabled="digFlag" placeholder="请输入..." style="width: 200px"></i-input>
+                 <FormItem label="费用项：" prop="feeTag">
+                    <i-input v-model="FeeInfo.feeTag"  :disabled="digFlag" placeholder="请输入..." style="width: 200px"></i-input>
+                </FormItem> 
             </Col>
           </Row>
           <Row style="margin-top:10px;">
-               <span>计算公式：</span><i-input :disabled="digFlag" placeholder="请输入..." style="width: 400px"></i-input>
+                <FormItem label="计算公式：" >
+                  <i-input :disabled="digFlag" @on-focus="feeFormuJs" v-model="showFormula1" placeholder="请输入..." style="width: 400px"></i-input>
+                </FormItem> 
           </Row>
           <Row style="margin-top:10px;">
               <Col span="12">
-                  <span>收取形式：</span>
-                 <Radio-group :disabled="digFlag" v-model="FeeInfo.feeWay">
-                    <Radio v-for="item in findAllReceivingForm" :disabled="digFlag" :label="item.value"><span>{{item.label}}</span></Radio>
-                    </Radio>
-                </Radio-group>
+                   <FormItem label="收取形式：" prop="feeWay">
+                      <Radio-group :disabled="digFlag" v-model="FeeInfo.feeWay">
+                          <Radio v-for="item in findAllReceivingForm" :disabled="digFlag" :label="item.value"><span>{{item.label}}</span></Radio>
+                          </Radio>
+                      </Radio-group>
+                    </FormItem>
               </Col>
           </Row>
         <Row style="margin-top:10px;">
             <Col span="24">
-                <span>收款方：&nbsp;&nbsp;&nbsp;</span>
+                <FormItem label="收款方：" prop="recCode">
                  <Radio-group v-model="FeeInfo.recCode">
                     <Radio  v-for="item in findAllRecipientParty" :disabled="digFlag" :label="item.value">
                         <span>{{item.label}}</span>
                     </Radio>
                 </Radio-group>
+                 </FormItem>
               </Col>
             </Col>
         </Row>
         <Row style="margin-top:10px;">
             <Col span="24">
-                <span>付款方：&nbsp;&nbsp;&nbsp;</span>
-                 <Radio-group :disabled="digFlag" v-model="FeeInfo.payCode">
-                    <Radio v-for="item in findAllPayer"  :disabled="digFlag" :label="item.value">
-                        <span>{{item.label}}</span>
-                    </Radio>
-                </Radio-group>
+                <FormItem label="付款方：" prop="payCode">
+                  <Radio-group :disabled="digFlag" v-model="FeeInfo.payCode">
+                      <Radio v-for="item in findAllPayer"  :disabled="digFlag" :label="item.value">
+                          <span>{{item.label}}</span>
+                      </Radio>
+                  </Radio-group>
+                </FormItem>
               </Col>
             </Row>
             <Row style="margin-top:10px;">
-                <Col span="2">
-                    <span>描述：</span>
-                </Col>
-                <Col span="16">
+                <Col span="18">
+                   <FormItem  label="描述：" prop="feeDesc">
                     <i-input :disabled="digFlag" v-model="FeeInfo.feeDesc" type="textarea" placeholder="Enter something..."></i-input>
+                    </FormItem>
                 </Col>
             </Row>
+            </Form>
              <div slot="footer">
                 <Button type="ghost" @click="cancelDig">取消</Button>
                 <Button v-show="!digFlag" @click="submitDig" type="primary">保存</Button>
@@ -161,6 +153,10 @@
   export default {
     data() {
       return {
+        // 系统函数
+        findAllSystemFunction:[],
+        // 系统变量
+        findAllSystemVariables:[],
         // 线上线下
         sqxs: '1',
         // 收款方
@@ -170,7 +166,9 @@
         // 描述
         ms: 'asd',
         //输入公式。
-        srgs: '1',
+        srgs: '',
+        // 传值
+        srgs1:'',
         //查看修改新增
         modal1: false,
         titleDig:'',
@@ -191,7 +189,18 @@
         digFeeName:'',
         digFeeType:'',
         // 新增 
-        FeeInfo:[],
+        FeeInfo:{
+            feeName:[],
+            feeType:[],
+            feeCategory:[],
+            feeTag:[],
+            feeWay:[],
+            recCode:[],
+            payCode:[],
+            feeDesc:[],
+            feeFormulaApiQO:{},
+            status:1
+        },
         // 控制弹框 可选
         digFlag:false,
         // 查询所有的付款方
@@ -200,14 +209,49 @@
         findAllRecipientParty:[],
         // 查询所有的收取方式
         findAllReceivingForm:[],
+        // 运算符号
+        ysfhJs:[],
+        // 运算保存 标识，
+        ysBcFlag:false,
         // 判断 是修改还是查看
+        showFormula1:'',
         isLookOrRe:'add',
         // 修改的code
         reCode:'',
         // 费用大类
         findAllCostCategory:[],
         
-        data8: []
+        data8: [],
+        // 校验
+        digValidate:{
+            feeName:[
+            
+                  { required: true, message: '不能为空', trigger: 'blur' }
+              
+            ],
+            feeType:[
+               { required: true, message: '不能为空', trigger: 'change' }
+            ],
+            feeCategory:[
+               { required: true, message: '不能为空', trigger: 'change' }
+            ],
+            feeTag:[
+               { required: true, message: '不能为空', trigger: 'blur' }
+            ],
+            feeWay:[
+                 { required: true, message: '不能为空', trigger: 'change' }
+            ],
+            recCode:[
+               { required: true, message: '不能为空', trigger: 'change' }
+            ],
+            payCode:[
+               { required: true, message: '不能为空', trigger: 'change' }
+            ],
+            feeDesc:[
+               { required: true, message: '不能为空', trigger: 'change' }
+            ],
+            
+        }
       };
     },
     computed: {
@@ -282,6 +326,7 @@
                         if(params.row.status==0){
                             this.$Message.error('禁用状态，不可修改')
                         }else{
+                            this.FeeInfo.status = params.row.status;
                             // 修改 
                             this.reCode = params.row.feeCode;
                             this.isLookOrRe ='re';
@@ -383,12 +428,59 @@
     },
     watch:{
         modal1(val){
-          if(!val){
-             // 清除弹框数据
+              if(!val){
+              if(!this.modal2){ 
+               this.$refs["FeeInfo"].resetFields();
+               }
           }
+        },
+        modal2(val){
+            if(!val){
+                this.ysBcFlag = false;
+                 this.srgs ='';
+            }
         }
     },
     methods: {
+     //计算公式
+      route(name){
+         
+         this.srgs += name.split('-')[0];
+         this.srgs1+= name.split('-')[1];
+         
+      },
+       
+      // 验证公式
+      vildGs(){
+          util.ajax({
+            url: "v1/feeFormula/formulaBudget",
+            method: "get",
+            params: {
+              formula: this.srgs
+            }
+          })
+          .then(res => {
+              console.log(res)
+              if(res.data.code==20000){
+                this.$Message.success(res.data.msg)
+                this.ysBcFlag = true;
+              }else{
+                this.$Message.error(res.data.msg)
+                this.ysBcFlag = false;
+              }
+          })
+      },
+      // 保存公式
+      submitGs(){
+          if(this.ysBcFlag==true){
+            this.modal2 = false;
+            this.modal1 = true;
+            this.showFormula1 = this.srgs;
+            this.FeeInfo.feeFormulaApiQO.formula = this.srgs1;
+          }else{
+             this.$Message.error('请验证！')
+          }
+      },
       // 表格数据
       findAllFeeList(index) {
         index = index || 1;
@@ -500,10 +592,41 @@
         // 保存逻辑
         if(this.isLookOrRe=='add'){
             //新增
+            
+            util.ajax('v1/feeInfo/AddFee', {
+              method: 'post',
+              params:this.FeeInfo
+            }).then(res => {
+              console.log(res)
+            })
         }else{
           // 修改
         
       }
+      },
+      // 计算公式弹框
+      feeFormuJs(){
+       
+          this.modal2 = true;
+          this.modal1 = false;
+          util.ajax('v1/feeInfo/findAllSystemVariables', {
+              method: 'get'
+            
+            }).then(res => {
+              this.findAllSystemVariables = res.data.data;
+            })
+          util.ajax('v1/feeInfo/findAllSystemFunction', {
+            method: 'get'
+          }).then(res => {
+            this.findAllSystemFunction = res.data.data;
+          })
+           util.ajax('v1/feeInfo/findAllOperation', {
+            method: 'get'
+          }).then(res => {
+            this.ysfhJs = res.data.data;
+          })
+          
+
       },
       // 查看弹框
       lookFindAllFee(){
@@ -521,6 +644,7 @@
                 this.digFlag = true;
                 this.titleDig = "查看费用";
                 this.modal1 = true;
+              
             })
            
           }
@@ -528,7 +652,7 @@
       },
       // 新增弹框
       addFindAllFee(){
-        
+              this.FeeInfo.status = 1;
               this.isLookOrRe = 'add';
               this.digFlag = false;
               this.titleDig = "新增费用";
