f0040f2757ca05e52b665e62056620508cb62c44
zhengjianli <zhengjianli126@qq.com>
2018-06-04 11:11:04 +0800

    接口代理

--- src/libs/http-axios.js
+++ src/libs/http-axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import {router} from '../router/index'
 import Cookies from 'js-cookie';
 let axiosAjax =  axios.create({
-    baseURL:'http://172.31.20.14:9999/',
+    baseURL:'http://172.31.20.14:9999/api',
     headers: {
         'Content-Type': 'application/json;charset=UTF-8',
     },
